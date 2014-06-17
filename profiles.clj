9a4f958e49601024efb25ead1847f2c8ef792231
Oscar Gonzalez Fernandez <ogfernandez@gmail.com>
2014-06-17 18:45:00 +0200

    Use stable version of tools.namespace
    Remove no longer needed repositories.

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,9 @@
-{:user {:dependencies [
-                       [clj-stacktrace "0.2.7"]
+{:user {:dependencies [[clj-stacktrace "0.2.7"]
                        [slamhound "1.5.0"]
                        [com.cemerick/pomegranate "0.2.0"]
                        [im.chit/vinyasa "0.1.2"]
-                       [org.clojure/tools.namespace "0.2.5-SNAPSHOT"]
-                       [spyscope "0.1.4"]
-                       ]
-        :repositories {"sonatype-oss-public"
-               "https://oss.sonatype.org/content/groups/public/"}
+                       [org.clojure/tools.namespace "0.2.4"]
+                       [spyscope "0.1.4"]]
         :plugins [[lein-difftest "2.0.0"]
                   [com.cemerick/austin "0.1.5-SNAPSHOT"]
                   #_[cider/cider-nrepl "0.7.0-SNAPSHOT"]
