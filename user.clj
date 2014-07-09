d9b86b8aef5054d8ef2409a89221f8bb1f32765e
Oscar Gonzalez Fernandez <Oscar.Fernandez@agentsmutual.co.uk>
2014-07-09 10:56:47 +0000

    Pomegranate was causing conflicts
    Slamhound is also injected with vinyasa

--- user.clj
+++ user.clj
@@ -1,47 +1,12 @@
-(require '[cemerick.pomegranate :as pomegranate]
-         '[cemerick.austin.repls :as repls]
-         '[cemerick.austin]
-         '[vinyasa.lein :refer [lein]]
-         '[vinyasa.inject]
-         '[clojure.tools.namespace.repl :refer [refresh]]
+(require '[vinyasa.inject]
          '[spyscope.core]
-         '[clojure.test :as t])
-
-
-(defn add-dependencies [& coordinates]
-  (pomegranate/add-dependencies
-   :coordinates (map #(cond
-                       (not (sequential? %)) [(symbol %) "RELEASE"]
-                       :else `[~(-> % first symbol) ~@(or (next %)
-                                                         ["RELEASE"])])
-                     coordinates)
-   :repositories (merge cemerick.pomegranate.aether/maven-central
-                        {"clojars"
-                         "http://clojars.org/repo"
-                         "sonatype-oss-public"
-                         "https://oss.sonatype.org/content/groups/public/"})))
+         '[clojure.test :as t]
+         '[slam.hound :refer [reconstruct swap-in-reconstructed-ns-form]])
 
 (ns user) ;; needed in order vinyasa.inject doesn't fail
 (vinyasa.inject/inject 'clojure.core '-
-                       '[[user add-dependencies]
-                         [clojure.tools.namespace.repl refresh]
+                       '[[slam.hound reconstruct swap-in-reconstructed-ns-form]
                          [clojure.repl apropos dir doc find-doc source pst root-cause]
                          [clojure.java.javadoc javadoc]
 
                          [clojure.pprint pprint]])
-
-(defn austin-phantomjs [& args]
-  (apply repls/exec :exec-cmds ["phantomjs"] args))
-
-(defn austin-chrome [& args]
-  (apply repls/exec :exec-cmds ["chromium-browser"] args))
-
-(defn austin-setup-browser-repl [& args]
-  (reset! repls/browser-repl-env
-          (apply cemerick.austin/repl-env args))
-  (repls/browser-connected-repl-js))
-
-(defn austin-browser-repl [& args]
-  (when-not @repls/browser-repl-env
-    (apply austin-setup-browser-repl args))
-  (repls/cljs-repl @repls/browser-repl-env))
