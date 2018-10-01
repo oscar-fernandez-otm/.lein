55f80b4431007b229340d3d2a384bb80fcd5b8e0
Oscar Gonzalez Fernandez <oscar.fernandez@agentsmutual.co.uk>
2018-10-01 18:55:44 +0100

    Move most of the plugins and customizations to the repl profile

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,16 @@
-{:user {:pedantic? :warn
-        :dependencies [[clj-stacktrace "0.2.8"]
-                       [zcaudate/lucid "1.4.4"]
+{:user {:dependencies [[clj-stacktrace "0.2.8"]]
+        :plugins      [[lein-difftest "2.0.0"]]}
+ :repl {:pedantic? :warn
+        :dependencies [[zcaudate/lucid "1.4.4"]
                        [org.clojure/tools.reader "1.3.0"]
                        [spyscope "0.1.7-SNAPSHOT" :exclusions [org.clojure/tools.reader]] ;; need to install locally
                        [com.google.guava/guava "20.0"]
                        [com.google.protobuf/protobuf-java "3.0.2"]
                        [org.clojure/tools.nrepl "0.2.13"]
                        [cider/piggieback "0.3.9"]]
-        :plugins [[lein-difftest "2.0.0"]
-                  [org.clojure/tools.nrepl "0.2.10"]
+        :plugins [[org.clojure/tools.nrepl "0.2.13"]
                   [cider/cider-nrepl "0.18.0"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.4.0"]]
         :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                        :init             (load-file (str
                                                      (System/getProperty "user.home")
