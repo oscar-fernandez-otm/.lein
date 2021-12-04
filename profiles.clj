0bc7c526ad8ff9f78fd2499d6a449dae7ecf7a50
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2021-12-04 21:19:59 +0000

    Updated .lein config

--- profiles.clj
+++ profiles.clj
@@ -1,24 +1,36 @@
-{:user {:plugins [[lein-difftest "2.0.0"]]}
+{:user {:plugins [[lein-difftest "2.0.0"]]
+        :dependencies [[clj-kondo "2021.04.23" :exclusions [com.cognitect/transit-clj com.cognitect/transit-java cheshire org.clojure/tools.reader]]]
+        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}
  :repl {:pedantic? :warn
-        :dependencies [#_[org.bouncycastle/bcprov-jdk15on "1.48"]
+
+
+        :dependencies [;; banners 
+;;                        [org.bouncycastle/bcprov-jdk15on "1.59"]
+                       ;; expert
+                       ;;                      [org.bouncycastle/bcprov-jdk15on "1.48"]
+                       
+                       [vvvvalvalval/scope-capture "0.3.2"]
                        [org.opensaml/xmltooling "1.4.4"]
                        [net.java.dev.jna/jna "4.2.2"]
                        [net.cgrand/macrovich "0.2.1"]
-                       [zcaudate/lucid "1.4.4" :exclusions [org.bouncycastle/bcprov-jdk15on zcaudate/lucid.package]]
-                       [org.clojure/tools.reader "1.3.2"]
-                       [spyscope "0.1.7-SNAPSHOT" :exclusions [org.clojure/tools.reader org.clojure/clojure]] ;; need to install locally
+                       [potemkin "0.4.5"]
+                       ;;[zcaudate/lucid "1.4.7" :exclusions [org.bouncycastle/bcprov-jdk15on zcaudate/lucid.package zcaudate/lucid.web]]
+                       [org.clojure/tools.reader "1.3.4"]
+                       [spyscope "0.1.7-SNAPSHOT" :exclusions [org.clojure/tools.reader org.clojure/clojure org.clojure/clojurescript]] ;; need to install locally
                        [com.google.errorprone/error_prone_annotations "2.1.3"]
-                       [com.google.guava/guava "22.0" :exclusions [com.google.code.findbugs/jsr305]]
+                       [com.google.guava/guava "25.1-jre" :exclusions [com.google.code.findbugs/jsr305]]
                        [com.google.protobuf/protobuf-java "3.0.2"]
                        [org.clojure/tools.logging "0.4.1"]
-                       [nrepl "0.6.0"]
+                       [nrepl "0.9.0-beta3"]
                        #_[cljs-analyzer "0.1.0-SNAPSHOT"]
-                       [cider/piggieback "0.4.2"]
+                       [cider/piggieback "0.5.3"]
+                       
                        [criterium "0.4.4"]]
-        :plugins [[cider/cider-nrepl "0.24.0"]
-                  [refactor-nrepl "2.5.0" :exclusions [org.clojure/clojure nrepl]]]
-        :repl-options {;;:nrepl-middleware [cider.piggieback/wrap-cljs-repl]
-                       :init             (load-file (str
-                                                     (System/getProperty "user.home")
+        :plugins [[cider/cider-nrepl "0.27.2"]
+                  [refactor-nrepl "3.0.0" :exclusions [org.clojure/clojure nrepl]]]
+        :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+                       :init              (load-file (str
+                                                      (System/getProperty "user.home")
                                                      "/.lein/user.clj"))}
-        :injections [(require 'spyscope.core)]}}
+        :injections [(require 'spyscope.core)
+                     (require 'sc.api)]}}
--- user.clj
+++ user.clj
@@ -1,14 +1,49 @@
-(require '[spyscope.core]
+(in-ns 'clojure.core)
+(require 'spyscope.repl
          '[clojure.test :as clojure-test]
-         '[lucid.core.inject :as inject]
-         '[lucid.core.debug :as debug]
-         'lucid.core.namespace
-         '[lucid.git :as git]
-         'lucid.mind)
+         'potemkin
+         'sc.api
+         'criterium.core
+         'clojure.repl
+         'clojure.java.javadoc)
 
-(ns user) ;; needed in order vinyasa.inject doesn't fail
+#_(try
+  (count (.getSupportedOptions
+          (doto (jdk.javadoc.doclet.StandardDoclet.)
+            (.init (java.util.Locale/getDefault) nil))))
+  (catch Throwable e
+    (println "workaround for java15" e)))
 
-(inject/in clojure.core - [clojure.repl apropos dir doc find-doc source pst root-cause]
+(let [former-ns (str *ns*)]
+  (try
+    (potemkin/import-fn clojure.repl/apropos -apropos)
+    (potemkin/import-macro clojure.repl/dir -dir)
+    (potemkin/import-macro clojure.repl/doc -doc)
+    (potemkin/import-fn clojure.repl/find-doc -find-doc)
+    (potemkin/import-macro clojure.repl/source -source)
+    (potemkin/import-fn clojure.repl/pst -pst)
+    (potemkin/import-fn clojure.repl/root-cause -root-cause)
+    (potemkin/import-fn clojure.java.javadoc/javadoc -javadoc)
+
+    (potemkin/import-macro criterium.core/bench -bench)
+    (potemkin/import-macro criterium.core/quick-bench -quick-bench)
+    (potemkin/import-macro criterium.core/with-progress-reporting -with-progress-reporting)
+
+    (potemkin/import-fn spyscope.repl/trace-query -trace-query)
+    (potemkin/import-fn spyscope.repl/trace-next -trace-next)
+    (potemkin/import-fn spyscope.repl/trace-clear -trace-clear)
+
+    (potemkin/import-macro sc.api/spy  -sc)
+    (potemkin/import-macro sc.api/letsc  -sc-letsc)
+    (potemkin/import-macro sc.api/defsc  -sc-defs)
+    
+    
+    (catch Throwable e
+      (println "Unexpected error:" e))
+    (finally
+      (in-ns (symbol former-ns)))))
+
+ #_(inject/in clojure.core - [clojure.repl apropos dir doc find-doc source pst root-cause]
            clojure.core - [clojure.pprint pprint print-table pp]
            clojure.core - [clojure.java.javadoc javadoc]
            clojure.core - [spyscope.repl trace-query trace-next trace-clear]
@@ -17,9 +52,9 @@
            clojure.core - [lucid.git git]
            clojure.core - [lucid.mind .% .%> .* .?])
 
-(inject/in clojure.core - [criterium.core bench quick-bench with-progress-reporting])
+#_(inject/in clojure.core - [criterium.core bench quick-bench with-progress-reporting])
 
-(try ;; try to inject logging functions, may not be loaded
+#_(try ;; try to inject logging functions, may not be loaded
   (require 'clojure.tools.logging)
   (inject/in clojure.core - [clojure.tools.logging info warn error])
   (catch Throwable e
