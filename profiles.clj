2231557c54a97b52cd3feee183eb2c99dd8dd474
Oscar Gonzalez Fernandez <oscar.fernandez@onthemarket.com>
2024-05-30 18:27:21 +0100

    Latest changes

--- profiles.clj
+++ profiles.clj
@@ -5,31 +5,32 @@
         :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]}}
  :repl {:pedantic? :warn
 
-
-        :dependencies [;; banners 
+        :dependencies [;; banners
 ;;                        [org.bouncycastle/bcprov-jdk15on "1.59"]
                        ;; expert
                        ;;                      [org.bouncycastle/bcprov-jdk15on "1.48"]
-                       
+
                        [vvvvalvalval/scope-capture "0.3.2"]
-                       [org.opensaml/xmltooling "1.4.4"]
-                       [net.java.dev.jna/jna "4.2.2"]
-                       [net.cgrand/macrovich "0.2.1"]
+                       #_[org.opensaml/xmltooling "1.4.4"]
+                       #_[net.java.dev.jna/jna "4.2.2"]
+                       #_[net.cgrand/macrovich "0.2.1"]
                        [potemkin "0.4.5"]
                        ;;[zcaudate/lucid "1.4.7" :exclusions [org.bouncycastle/bcprov-jdk15on zcaudate/lucid.package zcaudate/lucid.web]]
-                       [org.clojure/tools.reader "1.3.4"]
-                       [spyscope "0.1.7-SNAPSHOT" :exclusions [org.clojure/tools.reader org.clojure/clojure org.clojure/clojurescript]] ;; need to install locally
-                       [com.google.errorprone/error_prone_annotations "2.1.3"]
-                       [com.google.guava/guava "25.1-jre" :exclusions [com.google.code.findbugs/jsr305]]
-                       [com.google.protobuf/protobuf-java "3.0.2"]
-                       [org.clojure/tools.logging "0.4.1"]
-                       [nrepl "0.9.0-beta3"]
+                       [org.clojure/tools.reader "1.4.0"]
+                       [org.clojure/core.rrb-vector "0.2.0"]
+                       [spyscope "0.1.7-SNAPSHOT" :exclusions [org.clojure/tools.reader org.clojure/clojure org.clojure/clojurescript org.clojure/core.rrb-vector]] ;; need to install locally
+                       #_[com.google.errorprone/error_prone_annotations "2.1.3"]
+                       #_[com.google.guava/guava "25.1-jre" :exclusions [com.google.code.findbugs/jsr305]]
+                       #_[com.google.protobuf/protobuf-java "3.0.2"]
+                       #_[org.clojure/tools.logging "0.4.1"]
+                       [nrepl "1.1.1"]
                        #_[cljs-analyzer "0.1.0-SNAPSHOT"]
                        [cider/piggieback "0.5.3"]
-                       
+
                        [criterium "0.4.4"]]
-        :plugins [[cider/cider-nrepl "0.27.2"]
-                  [refactor-nrepl "3.0.0" :exclusions [org.clojure/clojure nrepl]]]
+        :plugins [[cider/cider-nrepl "0.47.0"]
+
+                  [refactor-nrepl "3.10.0" :exclusions [org.clojure/clojure nrepl]]]
         :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                        :init              (load-file (str
                                                       (System/getProperty "user.home")
